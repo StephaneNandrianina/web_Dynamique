e8340fd4af3ead9f79989b9e6ab9acb3edecbe60
StephaneNandrianina <119029511+StephaneNandrianina@users.noreply.github.com>
2023-03-07 08:46:48 +0100

    Add files via upload

---
+++ Mapping.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package etu1851.framework;
+
+/**
+ *
+ * @author itu
+ */
+public class Mapping {
+    String className;
+    String Method;
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getMethod() {
+        return Method;
+    }
+
+    public void setMethod(String Method) {
+        this.Method = Method;
+    }
+    
+}
